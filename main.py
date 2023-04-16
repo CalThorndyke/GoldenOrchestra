02791bd1ec6dc25edb78471c8fcadbafe133c56b
Luke Harris <6074660+luk707@users.noreply.github.com>
2023-04-16 20:28:28 +0100

    Play sound on sensor 0 press

--- main.py
+++ main.py
@@ -1,16 +1,7 @@
-# import os
-
-
-# def play_sound():
-#     os.system("aplay ../Music/police_s.wav")
-
-
-# play_sound()
-
-
-import time
 import board
 import busio
+import os
+import time
 
 # Import MPR121 module.
 import adafruit_mpr121
@@ -21,15 +12,20 @@ i2c = busio.I2C(board.SCL, board.SDA)
 # Create MPR121 object.
 mpr121 = adafruit_mpr121.MPR121(i2c)
 
-# Note you can optionally change the address of the device:
-# mpr121 = adafruit_mpr121.MPR121(i2c, address=0x91)
+# Play the sound file
+
+
+def play_sound():
+    os.system("aplay ./sound.wav")
+
+
+def is_touched():
+    return mpr121[0].value
+
 
 # Loop forever testing each input and printing when they're touched.
 while True:
-    # Loop through all 12 inputs (0-11).
-    for i in range(12):
-        # Call is_touched and pass it then number of the input.  If it's touched
-        # it will return True, otherwise it will return False.
-        if mpr121[i].value:
-            print("Input {} touched!".format(i))
+    if is_touched():
+        play_sound()
+
     time.sleep(0.25)  # Small delay to keep from spamming output messages.
