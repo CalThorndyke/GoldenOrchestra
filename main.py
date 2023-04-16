d7339b8565c0024d2298ea32c7950dad25ae3660
Luke Harris <6074660+luk707@users.noreply.github.com>
2023-04-16 20:56:53 +0100

    Support multiple pins different sounds

--- main.py
+++ main.py
@@ -13,25 +13,34 @@ i2c = busio.I2C(board.SCL, board.SDA)
 mpr121 = adafruit_mpr121.MPR121(i2c)
 
 
-def play_sound():
-    os.system("aplay ./sound.wav")
+def get_sound_for_pin(i: int) -> str:
+    match i:
+        case i if i in range(5):
+            return "Dizi.mp3"
+        case _:
+            return "Guzheng.mp3"
 
 
-def is_touched():
-    return any(mpr121[i].value for i in range(12))
+def play_sound_for_pin(i: int):
+    os.system(f"aplay ./{get_sound_for_pin(i)}")
+
+
+def is_touched(i: int):
+    return mpr121[i].value
 
 
 print("STARTED")
 
 # Loop forever testing each input and printing when they're touched.
 while True:
-    if is_touched():
-        print("DETECTED TOUCH")
-        play_sound()
+    for i in range(11):
+        if is_touched():
+            print("DETECTED TOUCH")
+            play_sound_for_pin(i)
 
-        while is_touched():
-            time.sleep(0.25)
+            while is_touched():
+                time.sleep(0.25)
 
-        print("RELEASED TOUCH")
+            print("RELEASED TOUCH")
 
     time.sleep(0.25)  # Small delay to keep from spamming output messages.
