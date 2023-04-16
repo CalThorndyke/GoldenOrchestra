007ca7f75d71c24c2ba894326e089f47396a8075
Luke Harris <6074660+luk707@users.noreply.github.com>
2023-04-16 20:44:56 +0100

    Add debug logs

--- main.py
+++ main.py
@@ -21,12 +21,17 @@ def is_touched():
     return any(mpr121[i].value for i in range(12))
 
 
+print("STARTED")
+
 # Loop forever testing each input and printing when they're touched.
 while True:
     if is_touched():
+        print("DETECTED TOUCH")
         play_sound()
 
         while is_touched():
             time.sleep(0.25)
+
+        print("RELEASED TOUCH")
 
     time.sleep(0.25)  # Small delay to keep from spamming output messages.
