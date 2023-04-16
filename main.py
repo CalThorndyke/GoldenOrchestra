40f86439b698c8992d74bfc71f1f8bc4cc15e21b
Luke Harris <6074660+luk707@users.noreply.github.com>
2023-04-16 20:21:05 +0100

    Add example from adafruit site

--- main.py
+++ main.py
@@ -1,8 +1,35 @@
-import os
+# import os
 
 
-def play_sound():
-    os.system("aplay ../Music/police_s.wav")
+# def play_sound():
+#     os.system("aplay ../Music/police_s.wav")
 
 
-play_sound()
+# play_sound()
+
+
+import time
+import board
+import busio
+
+# Import MPR121 module.
+import adafruit_mpr121
+
+# Create I2C bus.
+i2c = busio.I2C(board.SCL, board.SDA)
+
+# Create MPR121 object.
+mpr121 = adafruit_mpr121.MPR121(i2c)
+
+# Note you can optionally change the address of the device:
+# mpr121 = adafruit_mpr121.MPR121(i2c, address=0x91)
+
+# Loop forever testing each input and printing when they're touched.
+while True:
+    # Loop through all 12 inputs (0-11).
+    for i in range(12):
+        # Call is_touched and pass it then number of the input.  If it's touched
+        # it will return True, otherwise it will return False.
+        if mpr121[i].value:
+            print("Input {} touched!".format(i))
+    time.sleep(0.25)  # Small delay to keep from spamming output messages.
